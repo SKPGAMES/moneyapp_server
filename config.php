bff8d3e52cb0dd86b92b1771e49d19a09744a5c1
SKPGAMES <skpgames7749@gmail.com>
2025-09-27 13:21:29 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,14 @@
 <?php
-// Database config
-$host = "localhost";
-$user = "root";   // change if needed
-$pass = "";
-$dbname = "moneyapp";
+$host = "dpg-d3bpcrd6ubrc73e9qtkg-a";
+$port = "5432";
+$dbname = "moneyapp_db";
+$user = "moneyapp_db_user";
+$pass = "A2lVsaO78bKL9Z6C5Y9QimDijeFx9FYv";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-if ($conn->connect_error) {
-    die("DB Connection Failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die(json_encode(["status"=>"error","message"=>$e->getMessage()]));
 }
-
-session_start();
 ?>
